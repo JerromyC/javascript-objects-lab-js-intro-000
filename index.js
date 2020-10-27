2f006a7be0664444377a7992132b03c44765a9dd
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-27 19:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
+  var newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
 }
